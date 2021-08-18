063f7a70a5bd18983ed377bf39da20c6209e4f59
David Rock <david.martinelli@picpay.com>
2021-08-18 02:55:51 -0300

    create user repository and controller

---
+++ app/Repository/Eloquent/UserRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repository\Eloquent;
+
+use App\Models\User;
+use App\Repository\UserRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+
+class UserRepository extends BaseRepository implements UserRepositoryInterface
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * BaseRepository constructor.
+     *
+     * @param Model $model
+     */
+    public function __construct(User $model)
+    {
+        $this->model = $model;
+    }
+}
