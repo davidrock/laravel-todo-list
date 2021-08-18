6c7929be84d55798ac46192d8700f21d3627356a
David Rock <david.martinelli@picpay.com>
2021-08-18 11:09:54 -0300

    add JsonResult type return in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Repository\UserRepositoryInterface;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -14,7 +15,7 @@ class UserController extends Controller
         $this->userRepository = $userRepository;
     }
 
-    public function index()
+    public function index() : JsonResponse
     {
         return response()->json($this->userRepository->all(), 200);
     }
